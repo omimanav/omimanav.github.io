1fa454865823ef226d72850f5eace1e9345c17a5
omi <omimanav@icloud.com>
2019-02-10 17:54:29 +1300

    menu more space

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,7 +1,19 @@
-//$("div").has("img");
+$(".menuContainer").css({"display": "none"
+						 ,"opacity": "0"});
+$("div").css({"display": "none"
+			  ,"opacity": "0"});
+$("#loading").css("display", "block");
+
 window.onload = function() {
 	$("#loading").animate({
-		"opacity":"0"
-		,"z-index":"1"
+		"display":"none",
+	}, 500);
+	$(".menuContainer").animate({
+		"display": "block"
+		,"opacity": "1"
+	}, 500);
+	$("div").animate({
+		"display": "block"
+		,"opacity": "1"
 	}, 500);
 }
