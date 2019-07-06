42fa7f43f76fe9a3bc7291ac931421d264b69405
omi <omimanav@icloud.com>
2019-07-07 01:19:40 +1200

    fixes
    changed gallery display to javascript

---
+++ scripts/js.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+	if ($(window).width > 800) {
+		var images = $(".galleryBody > div:nth-child(2)").children().length
+		,galleryName = $(".galleryBody > div:nth-child(2)").attr("class")
+		,imageWidth = galleryName == "hebei02" ? 615 : (["d1010", "n1010"].indexOf(galleryName) > -1 ? 444 : 1083)
+		,width = images * imageWidth;
+		$(".galleryBody > div:nth-child(2)").css({
+			"width":width.toString()
+		});
+	}
+}
