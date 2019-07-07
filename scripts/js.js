8d3d05f4c780d7d6c1faee390d3eae74a057cc8f
omi <omimanav@icloud.com>
2019-07-08 11:05:04 +1200

    final final fix i swear
    local jq file
    images horizontal, js fix width()

--- scripts/js.js
+++ scripts/js.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	if ($(window).width > 800) {
+	if ($(document).width() > 800) {
 		var images = $(".galleryBody > div:nth-child(2)").children().length
 		,galleryName = $(".galleryBody > div:nth-child(2)").attr("class")
 		,imageWidth = galleryName == "hebei02" ? 615 : (["d1010", "n1010"].indexOf(galleryName) > -1 ? 444 : 1083)
@@ -7,5 +7,7 @@ window.onload = function() {
 		$(".galleryBody > div:nth-child(2)").css({
 			"width":width.toString()
 		});
+
+		console.log(images + "\n" + imageWidth);
 	}
 }
