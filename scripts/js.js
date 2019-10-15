4ee206034521b230bb887ad12b9a0463c1761094
omimanav <crazykiwicake@icloud.com>
2019-10-16 01:28:05 +1300

    dan

--- scripts/js.js
+++ scripts/js.js
@@ -5,7 +5,7 @@ window.onload = function() {
 		})
 		var images = $(".galleryBody > div:nth-child(2)").children().length
 		,galleryName = $(".galleryBody > div:nth-child(2)").attr("class")
-		,imageWidth = galleryName == "hebei02" ? 615 : (["d1010", "n1010"].indexOf(galleryName) > -1 ? 444 : 1083)
+		,imageWidth = galleryName == "hebei02" ? 615 : (["d1010", "n1010"].indexOf(galleryName) > -1 ? 444 : 1084)
 		,width = images * imageWidth;
 		$(".galleryBody > div:nth-child(2)").css({
 			"width":width.toString()
