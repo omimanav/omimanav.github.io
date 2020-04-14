b00e7a6063c3043f6cd5626dc2910abce980b402
omimanav <crazykiwicake@icloud.com>
2020-04-14 13:22:44 +1200

    newsite

--- scripts/js.js
+++ scripts/js.js
@@ -1,14 +1,39 @@
-window.onload = function() {
-	if ($(document).width() > 640) {
-		$(".galleryBody img").css({
-			"top":"calc(50vh - 300px)"
-		})
-		var images = $(".galleryBody > div:nth-child(2)").children().length
-		,galleryName = $(".galleryBody > div:nth-child(2)").attr("class")
-		,imageWidth = galleryName == "hebei02" ? 615 : (["d1010", "n1010"].indexOf(galleryName) > -1 ? 444 : 1084)
-		,width = images * imageWidth;
-		$(".galleryBody > div:nth-child(2)").css({
-			"width":width.toString()
-		});
+const gallerycount = {
+	"1010n":8
+	,"1010d":7
+	,"n-i":12
+	,"s-i":17
+	,"dxb":11
+	,"kef":13
+	,"seoul":19
+	,"gor":14
+	,"hebei01":13
+	,"hebei02":23
+	,"ind":15
+	,"sdc":11
+	,"portraits":11
+};
+function photoGallery(galleryname) {
+	var photourl = "https://omimanav.com/img/" 
+		+ galleryname 
+		+ "/" 
+		+ (galleryname.indexOf("hebei") == -1 ? galleryname : "hebei");
+	var photolist = [], key = [];
+	var i = galleryname == "hebei02" ? 14 : 0;
+	
+	for (i;i<gallerycount[galleryname]; i++) {
+		key.push(i);
+		photolist.push(photourl+i+".jpg");
 	}
+	
+	const photos = photolist.map((photo) => <img className="photo" src={photo}/>);
+	
+	ReactDOM.render(
+		photos,
+		$("#gallery")
+	);
+}
+
+function loader() {
+	$("#title").css("opacity","1");
 }
